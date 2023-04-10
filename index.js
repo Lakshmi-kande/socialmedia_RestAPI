2dbeb39a1e4c2f118f0de5bf082061f9f0e4b06b
Lakshmi-kande <kandelakshmi500@gmail.com>
2023-04-10 22:06:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
+const postRoute = require("./routes/posts")
+
+dotenv.config()
+
+const port=process.env.PORT || 8000;
+
+// const mongoose = require('mongoose');
+
+mongoose.connect(process.env.Mongo_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => console.log('Error connecting to database:', error));
+
+// mongoose.connect(process.env.Mongo_URL, {useNewUrlParser: true, useUnifiedTopology: true},
+//     ()=>{
+//     console.log("Connected to MongoDB")
+// })
+
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
+app.use("/api/posts", postRoute)
+
+
+app.listen(port,()=>{
+    console.log(`Backend server is running ${port}`)
+})
